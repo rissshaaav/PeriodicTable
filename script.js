e79914cc612c8f64dec8ef02ed475cbfe8bdafdb
Rishav <111266613+rissshaaav@users.noreply.github.com>
2023-03-06 12:10:04 +0530

    error in highlightByType function

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
 const toggleCard = (atomicNo) => {
     document.getElementById(atomicNo).classList.toggle('active');
-    console.log('clicked');
+}
+
+const sBlock = document.getElementsByClassName('s-block');
+const highlightS = () => {
+    for(let i=0; i<sBlock.length; i++){
+        sBlock[i].classList.toggle('highlight-s');
+    }
+}
+
+const pBlock = document.getElementsByClassName('p-block');
+const highlightP = () => {
+    for(let i=0; i<pBlock.length; i++){
+        pBlock[i].classList.toggle('highlight-p');
+    }
+}
+
+const dBlock = document.getElementsByClassName('d-block');
+const highlightD = () => {
+    for(let i=0; i<dBlock.length; i++){
+        dBlock[i].classList.toggle('highlight-d');
+    }
+}
+
+const fBlock = document.getElementsByClassName('f-block');
+const highlightF = () => {
+    for(let i=0; i<fBlock.length; i++){
+        fBlock[i].classList.toggle('highlight-f');
+    }
+}
+
+const highlightByType = () => {
+    let elementType = document.getElementById('element-type');
+    let selectedElementType = elementType.options[elementType.selectedIndex].value.replace(/ /g, "-").toLowerCase();
+    let elementList = document.getElementsByClassName(selectedElementType);
+    for(let i=0; i<elementList.length; i++){
+        elementList[i].classList.toggle('highlight-'+selectedElementType);
+    }
 }
